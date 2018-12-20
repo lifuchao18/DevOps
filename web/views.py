e1ddda33a48f8a679195cf43ed7b6652d4c61170
李付超 <lifuchao@jtoushou.com>
2018-12-20 18:44:12 +0800

    增加自动发布公钥模块

--- web/views.py
+++ web/views.py
@@ -1,4 +1,7 @@
 #encoding=utf-8
+import socket
+import os
+import pexpect
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect 
 from django.http import HttpResponse, HttpResponseRedirect
@@ -8,6 +11,56 @@ from django.core.urlresolvers import reverse
 
 from web.models import UserMenu, OsInfo
 # Create your views here.
+
+def ssh_keygen(user, m_ip, passwd):
+    if user == 'root':
+        if (os.path.exists("/root/.ssh/id_rsa.pub")):
+            cmd = "ssh-copy-id %s" %(m_ip)
+            child = pexpect.spawn(cmd)
+            index = child.expect(["password:","already exist","ERROR:", pexpect.TIMEOUT])
+            if index == 0:
+                child.sendline(passwd)
+                index = child.expect([pexpect.EOF])
+                if index == 0:
+                    child.close(force=True)
+                    return 1
+            elif index == 1:
+                child.close(force=True)
+                return 1
+            elif index == 2:
+                child.close(force=True)
+                return 0
+        else:
+            cmd = "ssh-keygen"
+            child = pexpect.spawn(cmd)
+            index = child.expect(["save the key", pexpect.TIMEOUT])
+            if index == 0:
+                child.sendline("\r")
+                index = child.expect(["Enter passphrase"])
+                if index == 0:
+                    child.sendline("\r")
+                    index = child.expect(["again:"])
+                    if index == 0:
+                        child.sendline("\r")
+                        index = child.expect([pexpect.EOF])
+                        if index == 0:
+                            child.close(force=True)
+                            print "END!"
+            cmd = "ssh-copy-id %s" %(m_ip)
+            child = pexpect.spawn(cmd)
+            index = child.expect(["password:","already exist","ERROR:", pexpect.TIMEOUT])
+            if index == 0:
+                child.sendline(passwd)
+                index = child.expect([pexpect.EOF])
+                if index == 0:
+                    child.close(force=True)
+                    return 1
+            elif index == 1:
+                child.close(force=True)
+                return 1
+            elif index == 2:
+                child.close(force=True)
+                return 0
 
 def userLogin(request):
     username = request.POST.get('username')
@@ -63,8 +116,15 @@ def OsCreat(request):
         ip = request.POST['ip']
         cpu = request.POST['cpu']
         mem = request.POST['mem']
-        OsInfo.objects.create(ip=ip,cpu=cpu,mem=mem)
-        return redirect('/oscreat/')
+        user = request.POST['user']
+        passwd = request.POST['password']
+        d = ssh_keygen(user=user,m_ip=ip,passwd=passwd)
+        if d == 1:
+            OsInfo.objects.create(ip=ip,cpu=cpu,mem=mem,ssh=True)
+            return redirect('/oscreat/')
+        else:
+            OsInfo.objects.create(ip=ip,cpu=cpu,mem=mem,ssh=False)
+            return redirect('/oscreat/')
     return render(request, 'access/os_creat.html')
 
 def OsUpdate(request,_id):
@@ -79,3 +139,20 @@ def OsUpdate(request,_id):
         osdata.save()
         return HttpResponseRedirect(reverse('osview'))
     return render(request,'access/os_update.html',context={'osdata':osdata})
+
+def get_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip
+
+def ssh(request):
+    data = OsInfo.objects.all()
+    iplist = []
+    m_ip = get_ip()
+    for i in data:
+        iplist.append(i.ip)
+    return render(request,'access/os_ssh.html',{'iplist': iplist, 'm_ip': m_ip})
